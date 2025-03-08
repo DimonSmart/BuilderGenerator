f5cbd67770153f88a2ccea7951896bd38e1ead5b
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2025-03-08 19:34:06 +0100

    Initial upload

---
+++ BuilderGenerator.Sample/Builders.cs
@@ -0,0 +1,20 @@
+﻿using BuilderGenerator.Runtime;
+
+namespace BuilderGenerator.Sample
+{
+    [GenerateBuilder]
+    public class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public Address? Address { get; set; }
+    }
+
+    [GenerateBuilder]
+    public class Address
+    {
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public Person? Person { get; set; }
+    }
+}
---
+++ BuilderGenerator.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BuilderGenerator.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = PersonBuilder.Create()
+               .Name("John Doe")
+               .Age(30)
+               .Address(address => address
+                   .Street("Main St")
+                   .City("Springfield"))
+               .Build();
+     
+            Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            Console.WriteLine($"Address: Street: {person.Address?.Street}, City: {person.Address?.City}");
+            Console.WriteLine($"Address.Parent is set correctly: {(person.Address?.Person == person ? "Yes" : "No")}");
+        }
+    }
+}
